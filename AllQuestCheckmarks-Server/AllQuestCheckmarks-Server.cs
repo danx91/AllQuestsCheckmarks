d8632f017f2a6cae0f15516f7347e005c816a9c4
Kobe Thuy <thuynguyentrungdang@gmail.com>
2025-11-26 02:27:40 +0700

    Updated Server component to 4.0

---
+++ AllQuestCheckmarks-Server/AllQuestCheckmarks-Server.cs
@@ -0,0 +1,50 @@
+﻿using SPTarkov.DI.Annotations;
+using SPTarkov.Server.Core.DI;
+using SPTarkov.Server.Core.Helpers;
+using SPTarkov.Server.Core.Models.Spt.Mod;
+using SPTarkov.Server.Core.Models.Utils;
+using SPTarkov.Server.Core.Servers;
+using Range = SemanticVersioning.Range;
+
+namespace AllQuestCheckmarks_Server;
+
+public record ModMetadata : AbstractModMetadata
+{
+    public override string ModGuid { get; init; } = "ZGFueDkx.AllQuestCheckmarks";
+    public override string Name { get; init; } = "All Quest Checkmarks Server";
+    public override string Author { get; init; } = "Pluto";
+    public override SemanticVersioning.Version Version { get; init; } = new("2.0.0");
+    public override Range SptVersion { get; init; } = new("~4.0.0");
+    public override string License { get; init; } = "MIT";
+    public override bool? IsBundleMod { get; init; } = true;
+    public override Dictionary<string, Range>? ModDependencies { get; init; }
+    public override string? Url { get; init; }
+    public override List<string>? Contributors { get; init; }
+    public override List<string>? Incompatibilities { get; init; }
+}
+
+[Injectable(TypePriority = OnLoadOrder.PostDBModLoader + 9000)]
+public class AllQuestCheckmarksServer(
+    ISptLogger<AllQuestCheckmarksServer> logger,
+    QuestHelper questHelper,
+    ConfigServer configServer,
+    ProfileHelper profileHelper,
+    CustomStaticRouter customStaticRouter) : IOnLoad
+{
+    public required QuestHelper QuestHelper;
+    public required ConfigServer ConfigServer;
+    public required ProfileHelper ProfileHelper;
+    
+    public Task OnLoad()
+    {
+        QuestHelper = questHelper;
+        ConfigServer = configServer;
+        ProfileHelper = profileHelper;
+        
+        customStaticRouter.PassData(QuestHelper, ConfigServer, ProfileHelper, logger);
+        
+        logger.Success("[AllQuestCheckmarksServer] Mod loaded successfully.");
+        
+        return Task.CompletedTask;
+    }
+}
